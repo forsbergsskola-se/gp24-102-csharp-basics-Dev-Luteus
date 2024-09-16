a67efd5495ddb6ce391fa106b293d614eee58a4c
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-09-16 14:33:03 +0200

    Init
    P10 Booleans

---
+++ P10Boolean/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("What's your age?");
+int age = Int32.Parse(Console.ReadLine());
+bool isChild = false;
+bool isTeenager = false;
+bool isAdult = false;
+
+if (age >= 0 && age <= 12)
+{
+    isChild = true;
+} else if (age >= 13 && age <= 19) {
+    isTeenager = true;
+} else if (age > 19) {
+    isAdult = true;
+}
+Console.WriteLine("You are a child: " + isChild);
+Console.WriteLine("You are a teenager " + isTeenager);
+Console.WriteLine("You are an adult: " + isAdult);
