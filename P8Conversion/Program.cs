a1f9fdf64c2c45a93864d2f1d8c6856ff24f70c3
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-09-16 13:24:55 +0200

    Init
    P8 Conversion

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices.JavaScript;
+
+Console.WriteLine("Give me a number:");
+string userInput = Console.ReadLine();
+
+float floatNumber = float.Parse(userInput); //Parse userInput to float
+Console.WriteLine(floatNumber);
+
+int intNumber = Convert.ToInt32(floatNumber); //Convert float to int
+intNumber = (int) floatNumber;
+Console.WriteLine(intNumber);
