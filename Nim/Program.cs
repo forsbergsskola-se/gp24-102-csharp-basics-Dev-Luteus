f277b8345c88021639f749006ac494c56680438b
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-09-18 13:00:34 +0200

    Added Main Menu
    Added Main Menu to Nim Game.
    You can now select if you want to play Nim Game (basic) version, or
    Nim Game (fun) version.

--- Nim/Program.cs
+++ Nim/Program.cs
@@ -1,56 +1,117 @@
-﻿Console.WriteLine("Welcome to Nim!");
-int matchesCount = 24; int playerMove = 0;
-bool playerTurn = false; bool aiTurn = false; 
-bool gameOver = false;
-Random Ai = new Random();
-nim:
-while (!gameOver) {
-    DisplayMatches(matchesCount);
-    Console.WriteLine("How many matches would you like to draw? (1-3)");
-    playerTurn:
-    playerTurn = true; aiTurn = false;
-    string input = Console.ReadLine();
-    if (!Int32.TryParse(input, out playerMove)) { //TryParse. If Parsing to Int not possible, Invalid input
-        Console.WriteLine("Invalid input! Please enter an integer.");
-        goto playerTurn;
-    }
-    switch (playerMove) {
-        case 1:
-        case 2:
-        case 3: //case 1-2-3 do the same things
-            if (playerMove > matchesCount) {
-                Console.WriteLine($"You can only draw up to {matchesCount} matches.");
-                goto playerTurn; 
+﻿using System;
+
+namespace NimConsoleApplication
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            bool exitProgram = false;
+            while (!exitProgram) { 
+                string mainMenu = @"
+        <> MAIN MENU <>
+-------------------------------
+    Please select an option
+    -----------------------
+    [A] - Nim Game (basic)
+    [B] - Nim Game (fun)
+    [Q] - Quit Program
+===============================";
+                Console.WriteLine(mainMenu);
+                string menuSelection = Console.ReadLine().ToUpper(); //Console.Readline returns string, change to var?
+
+                switch (menuSelection) {
+                    case "A":
+                        BasicNim();
+                        break;
+                    case "B":
+                        break;
+                    case "Q":
+                        exitProgram = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please select one of the appropriate options.");
+                        break;
+                }
             }
-            matchesCount -= playerMove; //update
-            break;
-        default:
-            Console.WriteLine("Please enter a valid amount! (1-3)");
-            goto playerTurn;
-    }
-    DisplayMatches(matchesCount); //End of player turn
-    if (matchesCount <= 0 && playerTurn == true) { gameOver = true; }
-    
-    if (matchesCount > 0) { //Prevent AI from taking a turn if Matches = 0
-        int aiMove = Ai.Next(1, 4);
-        Console.WriteLine("The AI draws " + aiMove + " matches.");
-        aiTurn = true; playerTurn = false;
-        matchesCount -= aiMove;
-        if (matchesCount <= 0 && aiTurn == true) {
-            gameOver = true;
         }
-    }
-    goto nim;
-}
-if (aiTurn == true && playerTurn == false) {
-    Console.WriteLine("The AI drew the last match. You win!");
-} else if (playerTurn == true && aiTurn == false) {
-    Console.WriteLine("You drew the last match. You lose!");
-}
-static void DisplayMatches(int matchesCount) { //function to display matches. (I have prior C# experience)
-    if (matchesCount > 0) {
-        Console.WriteLine(new String('|', matchesCount)+ $" ({matchesCount})"); //new string, char, amount, + counter
-    } else {
-        Console.WriteLine($"Matches left: ({matchesCount})");
+
+        static void BasicNim()
+        {
+            bool backToMenu = false;
+            while (!backToMenu) {
+                Console.WriteLine("You're inside of Nim Game (basic)!");
+                Console.WriteLine("Press 'C' to Confirm. Press 'Q' to go to Main Menu.");
+
+                string menuSelection = Console.ReadLine().ToUpper();
+                if (menuSelection == "C") { //If instead of switch for fun
+                    Console.WriteLine("Welcome to Nim!");
+                    int winCount = 0; int lostCount = 0;
+                    Console.WriteLine($@"You've won {winCount} times. You've lost {lostCount} times.");
+                    int matchesCount = 24; int playerMove = 0;
+                    bool playerTurn = false; bool aiTurn = false; 
+                    bool gameOver = false;
+                    Random Ai = new Random();
+                    nim:
+                    while (!gameOver) {
+                        DisplayMatches(matchesCount);
+                        Console.WriteLine("How many matches would you like to draw? (1-3)");
+                        playerTurn:
+                        playerTurn = true; aiTurn = false;
+                        string input = Console.ReadLine();
+                        if (!Int32.TryParse(input, out playerMove)) { //TryParse. If Parsing to Int not possible, Invalid input
+                            Console.WriteLine("Invalid input! Please enter an integer.");
+                            goto playerTurn;
+                        }
+                        switch (playerMove) {
+                            case 1:
+                            case 2:
+                            case 3: //case 1-2-3 do the same things
+                                if (playerMove > matchesCount) {
+                                    Console.WriteLine($"You can only draw up to {matchesCount} matches.");
+                                    goto playerTurn; 
+                                }
+                                matchesCount -= playerMove; //update
+                                break;
+                            default:
+                                Console.WriteLine("Please enter a valid amount! (1-3)");
+                                goto playerTurn;
+                        }
+                        DisplayMatches(matchesCount); //End of player turn
+                        if (matchesCount <= 0 && playerTurn == true) { gameOver = true; }
+                        
+                        if (matchesCount > 0) { //Prevent AI from taking a turn if Matches = 0
+                            int aiMove = Ai.Next(1, 4);
+                            Console.WriteLine("The AI draws " + aiMove + " matches.");
+                            aiTurn = true; playerTurn = false;
+                            matchesCount -= aiMove;
+                            if (matchesCount <= 0 && aiTurn == true) {
+                                gameOver = true;
+                            }
+                        }
+                        goto nim;
+                    }
+                    if (aiTurn == true && playerTurn == false) {
+                        Console.WriteLine("The AI drew the last match. You win!");
+                        winCounter++;
+                    } else if (playerTurn == true && aiTurn == false) {
+                        Console.WriteLine("You drew the last match. You lose!");
+                        loseCounter++;
+                    }
+                    static void DisplayMatches(int matchesCount) { //function to display matches. (I have prior C# experience)
+                        if (matchesCount > 0) {
+                            Console.WriteLine(new String('|', matchesCount)+ $" ({matchesCount})"); //new string, char, amount, + counter
+                        } else {
+                            Console.WriteLine($"Matches left: ({matchesCount})");
+                        }
+                    }
+                } else if (menuSelection == "Q") {
+                    backToMenu = true;
+                    Console.Clear();
+                } else {
+                    Console.WriteLine("Invalid Input. Please enter 'C' to Confirm, or 'Q' to go to Main Menu.");
+                }
+            }
+        }
     }
 }
