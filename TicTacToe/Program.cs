90ae266a0acf0013d1f9ceeeaf55fc7afa4b42fa
Dev-Luteus <EmilDev.Luteus@gmail.com>
2024-09-25 14:06:58 +0200

    Added Board Function
    Created 2D Array, looped to print 3x3.
    
    Modified loop to adjust colours for Even and Odd cells
    
    Made the cells wider by adding print padding.
    
    Made cells taller by adding a for-loop to print more rows, then print array row in the middle row.
    
    Added Public Class, Made Board Function to add organisation to code.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.Write("hi");
+﻿using System.Data;
+
+public class TicTacToe()
+{   
+    static int[,] cells = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} }; //static to call inside static methods/functions
+    static void Main() {
+        Console.WriteLine("Welcome to Tic-Tac-Toe \n" + "Whenever you're ready, press anything to continue! ...\n");
+        Console.ReadLine();
+        Board();
+    }
+    static void Board()
+    {
+        Console.Clear();
+        for (int rowNumber = 0; rowNumber < cells.GetLength(0); rowNumber++) 
+        {
+            for (int cellHeight = 0; cellHeight < 3; cellHeight++) {
+                   // I want each cell to be LARGER than a simple number. I use cellHeight to control how each cell is printed.
+                
+                for (int colNumber = 0; colNumber < cells.GetLength(1); colNumber++)
+                {  // I want to change the colour of cells, to make a "Checkered" look. Odd and Even does it well.
+                    
+                    if ((rowNumber + colNumber) % 2 == 0) { // Even
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        
+                    } else {                                // Odd
+                        Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    }
+                    
+                    // Print Number in the Middle of the cell (0, 1, 2)
+                    if (cellHeight == 1) {
+                        Console.ForegroundColor = ConsoleColor.White; // White is black..     ..?
+                        Console.Write("     " + (cells[rowNumber, colNumber] + "     "));
+                    } else {
+                        Console.Write("           ");
+                    }
+                }
+                Console.ResetColor();
+                Console.WriteLine(); //New Row
+            }
+        }
+    }
+    
+}
